5aea934bba38e987dcc1462ba069e7cf3c57511e
Felipe Cechin <ficechin@gmail.com>
2022-11-12 03:34:27 -0300

    added template components

---
+++ src/components/shared/template/Header/index.tsx
@@ -0,0 +1,31 @@
+import { IoMdFootball } from 'react-icons/io'
+import { useAuth } from '@/contexts/AuthContext';
+
+interface IHeaderProps {
+    children: React.ReactNode
+}
+
+function Header({ children }: IHeaderProps): JSX.Element {
+    const { signout } = useAuth();
+
+    return (
+        <header className='w-full bg-gradient-to-r from-emerald-700 to-green-500 px-4 py-4'>
+            <div className='max-w-7xl flex flex-col mx-auto pb-32'>
+                <div className='rounded-lg bg-white flex items-center justify-between px-4 py-4'>
+                    <span className='flex items-center normal-case text-xl font-bold text-emerald-800'>
+                        <IoMdFootball className='h-6 w-6 mr-1 self-center flex-shrink-0' />
+                        Meu time
+                    </span>
+                    <button className='link link-hover text-emerald-700 hover:text-emerald-900' onClick={signout}>
+                        Sair
+                    </button>
+                </div>
+                <h1 className='mt-12 text-3xl font-bold text-white flex items-center'>
+                    {children}
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/shared/template/MainContent/index.tsx
@@ -0,0 +1,15 @@
+interface IMainContentProps {
+    children: React.ReactNode
+}
+
+function MainContent({ children }: IMainContentProps): JSX.Element {
+    return (
+        <main className='-mt-14 px-4 pb-12'>
+            <div className='max-w-7xl mx-auto'>
+                {children}
+            </div>
+        </main>
+    )
+}
+
+export default MainContent;
