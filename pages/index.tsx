fca728ddf154cdcca790fb210667d9553f8123f8
Felipe Cechin <ficechin@gmail.com>
2022-11-12 03:35:04 -0300

    updated initial page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,188 @@
-export default function Home(): JSX.Element {
+import { FaChartBar } from 'react-icons/fa';
+import Footer from '@/components/shared/template/Footer';
+import { GetServerSideProps } from 'next';
+import Header from '@/components/shared/template/Header';
+import MainContent from '@/components/shared/template/MainContent';
+import MyTeamContentPage from '@/components/system/index/MyTeamContentPage';
+import { withSSRAuth } from '@/utils/withSSRAuth';
+
+export default function MyTeam(): JSX.Element {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <Header>
+                <FaChartBar className='flex-shrink-0 mr-2' />
+                Estatísticas
+            </Header>
+            <MainContent>
+                <MyTeamContentPage />
+            </MainContent>
+            <Footer />
+        </>
     )
 }
+
+export const getServerSideProps: GetServerSideProps = withSSRAuth(async ({ token }) => {
+    const countries = [
+        {
+            'name': 'Albania',
+            'code': 'AL',
+            'flag': 'https://media.api-sports.io/flags/al.svg'
+        },
+        {
+            'name': 'Algeria',
+            'code': 'DZ',
+            'flag': 'https://media.api-sports.io/flags/dz.svg'
+        },
+        {
+            'name': 'Andorra',
+            'code': 'AD',
+            'flag': 'https://media.api-sports.io/flags/ad.svg'
+        },
+        {
+            'name': 'Angola',
+            'code': 'AO',
+            'flag': 'https://media.api-sports.io/flags/ao.svg'
+        },
+        {
+            'name': 'Argentina',
+            'code': 'AR',
+            'flag': 'https://media.api-sports.io/flags/ar.svg'
+        },
+        {
+            'name': 'Armenia',
+            'code': 'AM',
+            'flag': 'https://media.api-sports.io/flags/am.svg'
+        },
+        {
+            'name': 'Aruba',
+            'code': 'AW',
+            'flag': 'https://media.api-sports.io/flags/aw.svg'
+        },
+        {
+            'name': 'Australia',
+            'code': 'AU',
+            'flag': 'https://media.api-sports.io/flags/au.svg'
+        },
+        {
+            'name': 'Austria',
+            'code': 'AT',
+            'flag': 'https://media.api-sports.io/flags/at.svg'
+        },
+        {
+            'name': 'Azerbaidjan',
+            'code': 'AZ',
+            'flag': 'https://media.api-sports.io/flags/az.svg'
+        },
+        {
+            'name': 'Bahrain',
+            'code': 'BH',
+            'flag': 'https://media.api-sports.io/flags/bh.svg'
+        },
+        {
+            'name': 'Bangladesh',
+            'code': 'BD',
+            'flag': 'https://media.api-sports.io/flags/bd.svg'
+        },
+        {
+            'name': 'Barbados',
+            'code': 'BB',
+            'flag': 'https://media.api-sports.io/flags/bb.svg'
+        },
+        {
+            'name': 'Belarus',
+            'code': 'BY',
+            'flag': 'https://media.api-sports.io/flags/by.svg'
+        },
+        {
+            'name': 'Belgium',
+            'code': 'BE',
+            'flag': 'https://media.api-sports.io/flags/be.svg'
+        },
+        {
+            'name': 'Belize',
+            'code': 'BZ',
+            'flag': 'https://media.api-sports.io/flags/bz.svg'
+        },
+        {
+            'name': 'Benin',
+            'code': 'BJ',
+            'flag': 'https://media.api-sports.io/flags/bj.svg'
+        },
+        {
+            'name': 'Bermuda',
+            'code': 'BM',
+            'flag': 'https://media.api-sports.io/flags/bm.svg'
+        },
+        {
+            'name': 'Bhutan',
+            'code': 'BT',
+            'flag': 'https://media.api-sports.io/flags/bt.svg'
+        },
+        {
+            'name': 'Bolivia',
+            'code': 'BO',
+            'flag': 'https://media.api-sports.io/flags/bo.svg'
+        },
+        {
+            'name': 'Bosnia',
+            'code': 'BA',
+            'flag': 'https://media.api-sports.io/flags/ba.svg'
+        },
+        {
+            'name': 'Botswana',
+            'code': 'BW',
+            'flag': 'https://media.api-sports.io/flags/bw.svg'
+        },
+        {
+            'name': 'Brazil',
+            'code': 'BR',
+            'flag': 'https://media.api-sports.io/flags/br.svg'
+        },
+        {
+            'name': 'Bulgaria',
+            'code': 'BG',
+            'flag': 'https://media.api-sports.io/flags/bg.svg'
+        },
+        {
+            'name': 'Burkina-Faso',
+            'code': 'BF',
+            'flag': 'https://media.api-sports.io/flags/bf.svg'
+        },
+        {
+            'name': 'Burundi',
+            'code': 'BI',
+            'flag': 'https://media.api-sports.io/flags/bi.svg'
+        },
+        {
+            'name': 'Cambodia',
+            'code': 'KH',
+            'flag': 'https://media.api-sports.io/flags/kh.svg'
+        },
+    ]
+
+    const seasons = [
+        2008,
+        2009,
+        2010,
+        2011,
+        2012,
+        2013,
+        2014,
+        2015,
+        2016,
+        2017,
+        2018,
+        2019,
+        2020,
+        2021,
+        2022,
+        2023
+    ]
+
+    return {
+        props: {
+            countries,
+            seasons
+        }
+    }
+});
---
+++ src/components/system/index/MyTeamContentPage/index.tsx
@@ -0,0 +1,142 @@
+import { FaSearch } from 'react-icons/fa';
+import Image from 'next/image';
+
+export default function MyTeamContentPage(): JSX.Element {
+    return (
+        <div className='bg-white rounded-lg shadow-lg px-4 py-4 flex flex-col'>
+            <p className='font-bold text-emerald-800 text-lg'>
+                Primeiro, selecione um país.
+            </p>
+            <div className='relative py-4'>
+                <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+                    <FaSearch className='text-gray-400' />
+                </div>
+                <input className='border border-emerald-600 w-full max-w-sm pl-10 py-2 rounded-lg focus:outline-none focus:ring focus:ring-emerald-300' placeholder='País' type='text' />
+            </div>
+            <div className='overflow-y-auto h-72'>
+                <div className='grid grid-cols-2 sm:grid-cols-6 lg:grid-cols-8 gap-3'>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                    <div className='py-4 px-4 border border-emerald-800 shadow-lg shadow-emerald-200 bg-green-50 rounded-lg flex flex-col items-center justify-center'>
+                        <Image alt='svg' className={'rounded-lg'} height={100} src={'https://media.api-sports.io/flags/al.svg'} width={100} />
+                        <p className='text-center text-emerald-800 font-bold'>Albânia</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
