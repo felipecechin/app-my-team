80d03937594346509b5ff29c72af9541733af7f3
Felipe Cechin <ficechin@gmail.com>
2022-11-12 03:33:32 -0300

    signin page

---
+++ pages/signin.tsx
@@ -0,0 +1,24 @@
+import SigninForm from '@/components/system/signin/SigninForm';
+
+export default function Signin(): JSX.Element {
+    return (
+        <main>
+            <div className='flex min-h-screen h-fit py-4 bg-emerald-800 items-center'>
+                <div className='container px-4 mx-auto'>
+                    <div className='bg-white rounded-lg shadow sm:max-w-md sm:w-full sm:mx-auto sm-max:m-5 py-10 px-4 sm:px-10'>
+                        <div className='max-w-lg mx-auto'>
+                            <div className='text-center mb-8'>
+                                <h2 className='text-3xl md:text-4xl font-extrabold mb-2'>
+                                    Faça login
+                                </h2>
+                            </div>
+                            <SigninForm />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+
---
+++ src/components/system/signin/SigninForm/index.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useRef } from 'react'
+
+import { FaSignInAlt } from 'react-icons/fa';
+import { useAuth } from '@/contexts/AuthContext';
+
+export default function SigninForm(): JSX.Element {
+    const { signin } = useAuth();
+    const inputTokenRef = useRef<HTMLInputElement>(null)
+
+    const handleFormSubmit = useCallback((event: React.FormEvent<HTMLFormElement>): void => {
+        event.preventDefault()
+        if (inputTokenRef.current && inputTokenRef.current.value) {
+            signin(inputTokenRef.current.value)
+        }
+    }, [signin])
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <div className='mb-6'>
+                <label className='block mb-2 font-extrabold'>Chave</label>
+                <input
+                    className='inline-block w-full p-4 leading-6 text-lg font-extrabold placeholder-emerald-700 bg-white shadow border-2 border-emerald-700 rounded focus:outline-none focus:ring focus:ring-emerald-300'
+                    placeholder='Insira a chave de acesso'
+                    ref={inputTokenRef}
+                    type='text'
+                />
+            </div>
+            <button className='flex items-center justify-center w-full py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-emerald-800 hover:bg-emerald-900 border-3 border-emerald-900 shadow rounded transition duration-200'>
+                <FaSignInAlt className='w-6 h-6 mr-2' /> Entrar
+            </button>
+        </form>
+    )
+}
